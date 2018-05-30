243b336ba6a2531c3690dc4bf1ab1bf2c0fb4d1a
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-30 14:44:34 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
   def current_user(session)
-    @user = User.find(session[:id])
+    @user = User.find(session[:user_id])
   end
 
   def is_logged_in?(session)
-
+    if
   end
 end
